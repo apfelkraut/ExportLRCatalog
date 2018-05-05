8e21be0110f978e4086a07b2ee864326c3afb4a7
Apfelkraut <ccc@apfelkraut.org>
2018-05-05 23:05:13 +0200

    Enabling dry run by default and other minor changes to config

--- Config.sh
+++ Config.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 # Dry run to change nothing and just log actions
-DRYRUN=false
+DRYRUN=true
 
 # Single directory per collection that includes all parent collection groups or
 # create (sub)directory structure following the collection's hierachy
 FLATEXPORT=true
 
 # Rename files when exporting or leave original filename
-RENAME=true
+RENAME=false
 
 # Seperator to be used when multiple collection (group) names are combined to
-# a single directory name during export. Otherwise being ignored.
+# a single directory name during export (see FLATEXPORT). Otherwise being ignored.
 COLLECTIONHIERACHYSEP=" - "
 
 # Which collections shall not be exported? (e.g. the Quick Collection)
 BLACKLISTEDCOLLECTIONS="('21')"
 
-# Which years shall be exported in separate unspecified folders?
+# Which years shall be handled via separate export?
+# Note: Applies only to images that are not part of any collection (unspecified).
 YEARS="('2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018')"
 
 # Path to the LR catalog
